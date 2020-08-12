b12b25d4d866be99eb030a1fd11b0313a28734be
Isaac Alencar <alencarisaac3@gmail.com>
2020-08-12 19:16:40 -0300

    configuration of different ambients

--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,37 @@
 import path from "path";
+import "dotenv/config";
 
 // Utilizando a sintaxe mais antiga para exportar o objeto pois, o knex não consegue identificar a
 // sintaxe de export default;
-
 module.exports = {
-  client: "sqlite3",
-  connection: {
-    filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+  //Configurando a conexão com o banco de dados
+  test: {
+    client: "pg",
+    connection: "postgres://postgres:docker@localhost:5432/proffy_test",
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    useNullAsDefault: true,
   },
-  migrations: {
-    directory: path.resolve(__dirname, "src", "database", "migrations"),
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    useNullAsDefault: true,
   },
-  useNullAsDefault: true,
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    useNullAsDefault: true,
+  },
 };
